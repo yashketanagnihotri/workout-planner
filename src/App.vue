484d713500c7cb4100b5f08221f460a6eacf769f
yashketanagnihotri <78458048+yashketanagnihotri@users.noreply.github.com>
2024-07-08 00:45:17 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref, watch } from "vue";
+import axios from "axios";
+import ExcerciseDetails from "./components/ExcerciseDetails.vue";
+
+const apiData = ref(""); // Holds the fetched API data
+const bodyPart = ref(""); // Holds the selected body part
+
+const muscles = [
+  "back",
+  "cardio",
+  "chest",
+  "lower arms",
+  "lower legs",
+  "neck",
+  "shoulders",
+  "upper arms",
+  "upper legs",
+  "waist",
+];
+
+// Function to fetch exercises based on selected body part
+const fetchExercises = async () => {
+  try {
+    const response = await axios.get(
+      `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart.value}`,
+      {
+        headers: {
+          "x-rapidapi-key":
+            "4a163c275emsh186bfca3d170053p1b2b56jsnefb108ea58e8",
+          "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+        },
+      }
+    );
+    apiData.value = response.data; // Update the apiData ref with fetched data
+    console.log(apiData.value); // Log the fetched data for debugging
+  } catch (error) {
+    console.error("Error fetching exercises:", error);
+  }
+};
+
+// Watch for changes in bodyPart.value and fetch new exercises accordingly
+watch(bodyPart, () => {
+  if (bodyPart.value) {
+    fetchExercises();
+  }
+});
+
+// Function to update selected muscle when dropdown selection changes
+const updateSelectedMuscle = (e) => {
+  bodyPart.value = e.target.value; // Update bodyPart.value with the selected muscle
+};
+</script>
+
+<template>
+  <div class="px-48 py-12">
+    <h1 class="text-3xl mb-6">Choose the muscle you want to work</h1>
+    <select
+      @change="updateSelectedMuscle"
+      class="bg-slate-100 px-5 py-3 rounded-lg mb-5"
+    >
+      <option v-for="(muscle, index) in muscles" :value="muscle" :key="index">
+        {{ muscle }}
+      </option>
+    </select>
+    <ExcerciseDetails :details="apiData" />
+    <!-- <h1 v-if="bodyPart" v-for="names in apiData">{{ names.name }}</h1> -->
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ExcerciseDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="flex justify-between bg-slate-100 mb-5 p-10 rounded-lg border-2 border-transparent hover:border-orange-500 focus:border-orange-500 cursor-pointer transition duration-300 ease-in-out"
+    v-for="data in props.details"
+  >
+    <div>
+      <h1 class="text-xl mb-5">{{ data.name }}</h1>
+      <ol>
+        <li
+          v-for="instruction in data.instructions"
+          class="mb-3 text-sm pr-24 text-justify text-slate-600"
+        >
+          {{ instruction }}
+        </li>
+      </ol>
+    </div>
+    <div>
+      <img :src="data.gifUrl" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  details: Object,
+});
+
+const data = props.details;
+</script>
